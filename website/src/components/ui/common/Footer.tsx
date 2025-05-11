30e6101288ab85a801ee7254568099e772f58073
3w36zj6 <52315048+3w36zj6@users.noreply.github.com>
2025-05-12 06:03:50 +0900

    fix: フッターのスタイルを修正

--- website/src/components/ui/common/Footer.tsx
+++ website/src/components/ui/common/Footer.tsx
@@ -13,27 +13,32 @@ export const Footer = () => {
         <div class="prose max-w-screen-xl px-4 py-12 mx-auto space-y-8 overflow-hidden sm:px-6 lg:px-8">
           <div class="flex justify-center mt-8 space-x-6">
             <a
-              href={githubRepositoryUrl}
-              class="text-gray-400 hover:text-gray-500"
-            >
-              <span class="sr-only">GitHub</span>
-              <div class="w-6 h-6">
-                <GitHubIcon />
-              </div>
-            </a>
-            <a
               href={discordServerUrl}
-              class="text-gray-400 hover:text-gray-500"
+              class="text-gray-600 hover:text-gray-800 transition-colors"
             >
               <span class="sr-only">Discord</span>
               <div class="w-6 h-6">
                 <DiscordIcon />
               </div>
             </a>
+            <a
+              href={githubRepositoryUrl}
+              class="text-gray-600 hover:text-gray-800 transition-colors"
+            >
+              <span class="sr-only">GitHub</span>
+              <div class="w-6 h-6">
+                <GitHubIcon />
+              </div>
+            </a>
           </div>
-          <p class="mt-8 text-base leading-6 text-center text-gray-400">
+          <p class="mt-8 text-sm leading-6 text-center text-gray-600">
             Translated by{" "}
-            <a href={githubOrganizationUrl}>Typst Japan Community</a>
+            <a
+              href={githubOrganizationUrl}
+              class="text-gray-600 hover:text-gray-800 transition-colors"
+            >
+              Typst Japan Community
+            </a>
           </p>
         </div>
       </section>
